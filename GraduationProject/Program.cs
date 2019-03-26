f71cff525bd8f7ab991c9767e3b3b10a9b216a57
AbdAyyad <abdayyad2012@gmail.com>
2019-03-26 11:11:35 +0200

    Add project files.

---
+++ GraduationProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GraduationProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
